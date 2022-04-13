5afa5b43bb368e2e32e50e24460c3381b9c82dc7
Akeem <akeem.mighty@gmail.com>
2022-04-13 14:21:47 -0400

    Created Functionality to Filter Dishes based on Italian Dishes with Serving Size Greater than 5

--- app.js
+++ app.js
@@ -145,6 +145,22 @@ console.log('Vegetarian Foods: ', vegetarianFood);
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
 
+function problemTwo(){
+    
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Italian" && el.servings > 5){
+            return true;
+        }
+        else{
+            return false;
+        }});
+    
+    return results;
+    
+}
+let italianFoodGtFiveServings = problemTwo();
+console.log('Italian Foods Greater Than Five Servings: ', italianFoodGtFiveServings);
+
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 
